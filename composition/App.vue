004ef5eda0e32dbc33d9a21db3d01813abd77e8d
Ryan <ryan.stewart113@gmail.com>
2022-04-18 15:30:39 -0400

    checkpoint section 4.38

---
+++ composition/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <button @click="increment">{{ count }}</button>
+  <button @click="increase('a')">{{ numbers.a }}</button>
+  <button @click="increase('b')">{{ numbers.b }}</button>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const numbers = reactive({
+      a:0,
+      b:0
+    })
+
+    const increase = (n) => {
+      numbers[n] ++
+    }
+
+    const increment = () => {
+      count.value ++
+    }
+
+    return {
+      count,
+      increase,
+      increment,
+      numbers
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+button {
+  height: 100px;
+  width: 100px;
+  font-size: 40px;
+}
+</style>
